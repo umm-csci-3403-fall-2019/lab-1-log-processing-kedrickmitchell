7d0a2d8fb450911da28d9becde8338164cb0e8a1
hill1588 <hill1588@morris.umn.edu>
2019-09-10 11:35:41 -0500

    change to process_client_logs code

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -2,4 +2,10 @@
 
 DIR=$1
 
-sed -E 's/\* ([a-zA-Z0-9]):00:00 computer_name sshd [xxxxx]: Failed password fo invalid user ([a-zA-z]) from ([0-9].+) port 0000 ssh2//' 
+cd $DIR
+
+cat *.txt |
+
+#	sed -E 's/\* ([a-zA-Z0-9]):\[0-9]+\]:\[0-9]+\] computer_name sshd \[[0-9]+\]: Failed password for invalid user ([a-zA-z]) from ([0-9].+) port \[[0-9]+\] ssh2/\1 \2 \3/' <  > failed_login_data.txt
+
+
