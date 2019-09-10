8118471f7b8a01a795a1c19a064a755b1800653d
hill1588 <hill1588@morris.umn.edu>
2019-09-10 17:45:08 -0500

    process_client_logs runs successful, but not correctly

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -1,11 +1,12 @@
 #!/bin/bash	
 
 DIR=$1
+HERE=$(pwd)
 
 cd $DIR
 
-cat *.txt |
+touch temp.txt
 
-#	sed -E 's/\* ([a-zA-Z0-9]):\[0-9]+\]:\[0-9]+\] computer_name sshd \[[0-9]+\]: Failed password for invalid user ([a-zA-z]) from ([0-9].+) port \[[0-9]+\] ssh2/\1 \2 \3/' <  > failed_login_data.txt
+#cat *.txt > temp.txt | sed -En 's/\* ([a-zA-Z0-9 ]+):\[[0-9]+\]:\[[0-9]+\] computer_name sshd \[[0-9]+\]: Failed password for (invalid user)? ([a-zA-z]) from ([0-9].+) port \[[0-9]+\] ssh2/\1 \2 \3/p' < temp.txt >> failed_login_data.txt
 
-
+cat *.txt >> temp.txt | awk '{print $1} {print $2} {print $3} {print $11} {print $13}' temp.txt
