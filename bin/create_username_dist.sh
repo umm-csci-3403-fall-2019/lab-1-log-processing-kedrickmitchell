8b99d999e15012bfed4cb9036b2b4adeab588eb6
hill1588 <hill1588@morris.umn.edu>
2019-09-11 18:44:26 -0500

    fixes issues where process_client_logs.sh did not print the groups.

--- bin/create_username_dist.sh
+++ bin/create_username_dist.sh
@@ -2,11 +2,15 @@
 
 DIRNAME=$1
 
+#call wrap contents; header and footer from the html_components section and using this data collected from the failed_login_data.txt.
 
-#tar -xzf 
+#this file should count the number of occurences of failed login attempts by username, then create it into a file that contains javascript that has (data.addRow('Username', '# of occurences')) 
+
+cd $DIRNAME
+
+cat failed_login_data.txt | awk ' { print $4 }' failed_login_data.txt | uniq -fci
 
 
 
-#VAR1= sort :
 
-#uniq
+
--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -1,8 +1,9 @@
-#!/bin/bash	
+#!/bin/bash     
 
 DIR=$1
 
 cd $DIR
 
-cat var/log/* | sed -En 's/([a-zA-Z0-9 ]+):\[[0-9]+\]:\[[0-9]+\] computer_name sshd\[[0-9]+\]: Failed password for (invalid user)? ([a-zA-z0-9]+) from ([0-9.]+) port \[[0-9]+\] ssh2/\1 \3 \4/p' > failed_login_data.txt
+#cat var/log/* | sed -E -n 's/([a-zA-Z0-9 ]+):([0-9]+):([0-9]+) ([a-zA-Z_]+) sshd\[([0-9]+)\]: Failed password for (invalid user )? ([a-zA-z0-9]+) from ([0-9.]+) port ([0-9]+) ssh2/\1\7\8/p' > failed_login_data.txt
 
+cat var/log/* | awk 'match($0, /([a-zA-Z0-9 ]+):([0-9]+):([0-9]+) ([a-zA-Z_]+) sshd\[([0-9]+)\]: Failed password for (invalid user )? ([a-zA-z0-9]+) from ([0-9.]+) port ([0-9]+) ssh2/, groups) {print groups[1]groups[7]groups[8]}' > failed_login_data.txt
