411d2c49a099f9fb24db407a2eabf0229dd97194
hill1588 <hill1588@morris.umn.edu>
2019-09-11 21:52:43 -0500

    fixes issue where create_username_dist.sh does not print correct list

--- bin/create_username_dist.sh
+++ bin/create_username_dist.sh
@@ -9,16 +9,14 @@ MAIN=$(pwd)
 cd $DIRNAME
 
 HERE=$(pwd)
-touch temp.txt
 
-cat failed_login_data.txt | awk 'match($0,/([a-zA-Z]+) ([0-9 ]+) ([a-zA-z0-9_]+) ([0-9.]+)/, groups) {print groups[3] }' < failed_login_data.txt > temp.txt | sort temp.txt | uniq -fc | awk 'match($0,/([0-9]+) ([a-zA-Z _]+)/, groups) {print data.addRow([\x27groups[2]\x27,groups[1]]);}' 
 
-cd 
+cat */failed_login_data.txt | awk 'match($0, /([a-zA-Z]+) ([0-9]+) ([0-9]+) ([a-zA-z0-9_]+) ([0-9.]+)/, groups) {print groups[4]}' | sort | uniq -c | awk 'match($0, /[ ]+([0-9]+) ([a-zA-Z_0-9]+)/, groups) {print "data.addRow([\x27"groups[2]"\x27, "groups[1]"]);"}' > temp.html
+
 cd $MAIN
 
-./ bin/wrap_contents.sh html_components/username_dist_header.html $HERE/temp.txt html_components/username_dist_footer.html
+./bin/wrap_contents.sh $HERE/temp.html $MAIN/html_components/username_dist $HERE/username_dist.html
 
-
-
+rm $HERE/temp.html
 
 
