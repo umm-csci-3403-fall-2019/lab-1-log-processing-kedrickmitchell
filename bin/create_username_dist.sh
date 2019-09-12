597f1789da31f68f16ba1dacaee13d012b2075e7
hill1588 <hill1588@morris.umn.edu>
2019-09-11 21:58:26 -0500

    fixes issues in shellcheck for create_username_dist.sh

--- bin/create_username_dist.sh
+++ bin/create_username_dist.sh
@@ -6,17 +6,17 @@ DIRNAME=$1
 
 #this file should count the number of occurences of failed login attempts by username, then create it into a file that contains javascript that has (data.addRow('Username', '# of occurences')) 
 MAIN=$(pwd)
-cd $DIRNAME
+cd "$DIRNAME" || exit
 
 HERE=$(pwd)
 
 
 cat */failed_login_data.txt | awk 'match($0, /([a-zA-Z]+) ([0-9]+) ([0-9]+) ([a-zA-z0-9_]+) ([0-9.]+)/, groups) {print groups[4]}' | sort | uniq -c | awk 'match($0, /[ ]+([0-9]+) ([a-zA-Z_0-9]+)/, groups) {print "data.addRow([\x27"groups[2]"\x27, "groups[1]"]);"}' > temp.html
 
-cd $MAIN
+cd "$MAIN" || exit
 
-./bin/wrap_contents.sh $HERE/temp.html $MAIN/html_components/username_dist $HERE/username_dist.html
+./bin/wrap_contents.sh "$HERE"/temp.html "$MAIN"/html_components/username_dist "$HERE"/username_dist.html
 
-rm $HERE/temp.html
+rm "$HERE/temp.html"
 
 
