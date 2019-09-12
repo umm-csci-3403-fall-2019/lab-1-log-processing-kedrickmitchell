d23c04be6166513c7bb90bc948c0bfa74a61d6e4
hill1588 <hill1588@morris.umn.edu>
2019-09-11 22:01:14 -0500

    fixes issues with shellcheck in process_client_logs

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -2,7 +2,7 @@
 
 DIR=$1
 
-cd $DIR
+cd "$DIR" || exit
 
 #cat var/log/* | sed -E -n 's/([a-zA-Z0-9 ]+):([0-9]+):([0-9]+) ([a-zA-Z_]+) sshd\[([0-9]+)\]: Failed password for (invalid user )? ([a-zA-z0-9]+) from ([0-9.]+) port ([0-9]+) ssh2/\1\7\8/p' > failed_login_data.txt
 
