55702917f24a94dca0cc0d8f6d26ebf3a8807965
hill1588 <hill1588@morris.umn.edu>
2019-09-17 11:34:33 -0500

    Update to assemble_report.sh

--- bin/assemble_report.sh
+++ bin/assemble_report.sh
@@ -12,7 +12,7 @@ cat country_dist.html hours_dist.html username_dist.html > temp.html
 
 cd "$main" || exit 
 
-./bin/wrap_contents.sh "$here"/temp.html "$main"/html_components/summary_plots "$here"/failed_login_summary.html
+./bin/wrap_contents.sh "$here"/temp.html html_components/summary_plots "$here"/failed_login_summary.html
 
 rm "$here/temp.html"
 
